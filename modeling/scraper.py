0c6d8e6e6f4c47177177b2a805f76f257b9941ea
Sotiris Tzamaras <sotiris@tzamaras.com>
2022-12-02 22:51:02 +0200

    add documentation to LinkedInScraper class

--- modeling/scraper.py
+++ modeling/scraper.py
@@ -3,19 +3,34 @@ from database import MongoDB
 import json
 
 if __name__ == "__main__":
-    with open("../credentials.json",'r') as secrets:
-        creds = json.load(secrets)
-
+    # The search keywords used for scraping job posts
     role = "Data Scientist"
     location = "Athens, Attiki, Greece"
 
+    # Regex expressions to match desired job titles
+    regex_matches = {
+        'Data Scientist': [r'.*[Dd]ata.?[Ss]cien.*'],
+        'Data Analyst':   [r'.*[Dd]ata.?[Aa]nalyst.*'],
+        'ML Engineer':    [r'.*[Mm]achine.?[Ll]earning.*', r'.*[Mm][Ll].?[Ee]ngineer.*'],
+        'Deep Learning':  [r'.*[Dd]eep.?[Ll]earning.*'],
+        'AI Engineer':    [r'.*[Aa]rtificial.?[Ii]ntelligence.*', r'(?:^|(?<=[\s]))\(?[Aa][Ii]\)?(?=[\s]|$)'],
+        'MLOps':          [r'.*[Mm][Ll].?[Oo]ps']
+    }
+
+    # Read the secret credentials for login
+    with open("../credentials.json",'r') as secrets:
+        creds = json.load(secrets)
+
+    # Initialize the MongoDB database
     database = MongoDB()
 
+    # List of created scrapers
     scrapers = [
         LinkedInScraper(creds['username'], creds['password'])
     ]
 
+    # Perform scraping from all available websites
     for scraper in scrapers:
-        scraped_jobs = scraper.get_jobs(role, location)
+        scraped_jobs = scraper.get_jobs(role, location, regex_matches)
         if scraped_jobs:
             database.insert_documents(scraped_jobs)
--- modeling/scrapers/LinkedInScraper.py
+++ modeling/scrapers/LinkedInScraper.py
@@ -11,7 +11,20 @@ TIMEOUT = 2.5
 
 class LinkedInScraper:
     '''
-    Constructor for the ``Edge`` driver with parameters.
+    Creates a new instance of the LinkedIn scraper.
+
+    Args:
+    -------
+    - `username` (str): The username used to login to LinkedIn
+    - `password` (str): The password used to login to LinkedIn
+
+    Methods:
+    -------
+    - `login()`: Performs the login to LinkedIn
+    - `infinite_scroll()`: Scrolls to the end of the page
+    - `filter_job()`: Uses regex to match title of job
+    - `extract_job_data`: Formats the data from each job into a dictionary
+    - `get_jobs`: Main scraping method which automates the procedure
     '''
 
     def __init__(self, username, password):
@@ -30,7 +43,14 @@ class LinkedInScraper:
         self.username = username
         self.password = password
 
-    def login(self):
+    def login(self) -> bool:
+        '''
+        Performs the login process to LinkedIn.
+
+        Returns:
+        -------
+        - `bool`: True if login was successful, otherwise False
+        '''
         self.driver.get(self.url_index)
         WebDriverWait(self.driver,5).until(EC.visibility_of_all_elements_located((By.ID,"session_key")))
         self.driver.find_element(By.ID, 'session_key').send_keys(self.username)
@@ -46,44 +66,66 @@ class LinkedInScraper:
         return True
 
     def infinite_scroll(self) -> bool:
+        '''
+        Scrolls down to the end of the page.
+
+        Returns:
+        -------
+        - `bool`: If scroll was successful returns True. When the end of the page is found returns False
+        '''
+
         # Get scroll height
         last_height = self.driver.execute_script("return document.body.scrollHeight")
 
+        # Check if show more button is present and click it
         infinite_scroller_btn = self.driver.find_elements(By.CLASS_NAME, "infinite-scroller__show-more-button--visible")
         if infinite_scroller_btn:
             infinite_scroller_btn[0].click()
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(TIMEOUT)
 
-        # Calculate new scroll height and compare with last scroll height
+        # Calculate new scroll height and compare with last value
         new_height = self.driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             return False
+
         last_height = new_height
 
         return True
 
-    def filter_job(self, job_role: str) -> list:
+    def filter_job(self, regex_matches: dict, job_role: str) -> list:
+        '''
+        Tries to match a given job title against a list of regular expressions.
 
-        re_matches = {
-            'Data Scientist': [r'.*[Dd]ata.?[Ss]cien.*'],
-            'Data Analyst':   [r'.*[Dd]ata.?[Aa]nalyst.*'],
-            'ML Engineer':    [r'.*[Mm]achine.?[Ll]earning.*', r'.*[Mm][Ll].?[Ee]ngineer.*'],
-            'Deep Learning':  [r'.*[Dd]eep.?[Ll]earning.*'],
-            'AI Engineer':    [r'.*[Aa]rtificial.?[Ii]ntelligence.*', r'(?:^|(?<=[\s]))\(?[Aa][Ii]\)?(?=[\s]|$)'],
-            'MLOps':          [r'.*[Mm][Ll].?[Oo]ps']
-        }
+        Args:
+        -------
+        - `regex_matches` (dict): Dictionary containing regular expressions for each job title
+        - `job_role` (str): The title or role of the job
+
+        Returns:
+        -------
+        - `list`: List containing all matched job titles. May be empty if no matches were made.
+        '''
 
         roles = []
-        for role in re_matches:
-            for reg_ex in re_matches[role]:
+        for role in regex_matches:
+            for reg_ex in regex_matches[role]:
                 if re.search(reg_ex, job_role) != None:
                     roles.append(role)
         return roles
 
-    def get_job_list(self, url: str) -> list:
+    def get_job_list(self, url: str, regex_matches: dict) -> list:
         '''
-        Documentation missing
+        Collects all available job posts.
+
+        Args:
+        -------
+        - `url` (str): The url of the job post
+        - `regex_matches` (dict): Dictionary containing regular expressions for each job title. Used for the `filter_job` method
+
+        Returns:
+        -------
+        - `list`: A collection of scraped job posts with their url, id and titles
         '''
         
         print('LinkedIn Scraper | INFO: Gathering job posts. Please wait...\n')
@@ -103,8 +145,8 @@ class LinkedInScraper:
                 current_job_index += 1
                 try:
                     job_path = f'//*[@id="main-content"]/section[@class="two-pane-serp-page__results-list"]/ul/li[{current_job_index}]/div'
-                    job_roles = self.filter_job(self.driver.find_element(By.XPATH, job_path + '/div[2]/h3').text)
-                    if len(job_roles) == 0:
+                    job_roles = self.filter_job(regex_matches, self.driver.find_element(By.XPATH, job_path + '/div[2]/h3').text)
+                    if not job_roles:
                         continue
                     job_url = self.driver.find_element(By.XPATH, job_path + '/a').get_attribute('href')
                     job_id = self.driver.find_element(By.XPATH, job_path).get_attribute('data-entity-urn').split(":")[-1]
@@ -125,6 +167,18 @@ class LinkedInScraper:
         return job_list
 
     def extract_job_data(self, job_list: list):
+        '''
+        Scraps and extracts the information about a specific job post.
+
+        Args:
+        -------
+        - `job_list` (list): A collection of scraped job posts with their url, id and titles
+
+        Returns:
+        -------
+        - `list`: A collection containing information about the gathered job posts. Each element is a `dict`
+        '''
+
         job_data = []
         job_info_section = f'//div[@role="main"]/div[1]/div/div/div[1]'
 
@@ -144,12 +198,12 @@ class LinkedInScraper:
                 
                 job_type = self.driver.find_element(By.XPATH, job_info_section + '/div[2]/ul/li[1]/span').text.split(" · ")
                 job["type"] = job_type[0]
-                if len(job_type) > 1:
+                if job_type:
                     job["level"] = job_type[1]
 
                 job_insights = self.driver.find_element(By.XPATH, job_info_section + '/div[2]/ul/li[2]/span').text.split(" · ")
                 job["company_size"] = job_insights[0].removesuffix(' employees')
-                if len(job_insights) > 1:
+                if job_insights:
                     job["industry"] = job_insights[1]
 
                 try:
@@ -168,14 +222,29 @@ class LinkedInScraper:
 
         return job_data
 
-    def get_jobs(self, role: str, location: str) -> (list | bool):
+    def get_jobs(self, role: str, location: str, regex_matches: dict) -> (list | bool):
+        '''
+        Performs the necessary steps to scrap data from LinkedIn given a job title and location.
+
+        Args:
+        -------
+        - `role` (str): A collection of scraped job posts with their url, id and titles
+        - `location` (str): A collection of scraped job posts with their url, id and titles
+        - `regex_matches` (dict): Dictionary containing regular expressions for each job title
+
+        Returns:
+        -------
+        - `list`: A collection containing information about the gathered job posts. Each element is a `dict`
+        - `bool`: Returns False if either no job posts were found or the login failed
+        '''
+
         role = role.replace(" ", "%20")
         location = location.replace(", ", "%2C%20")
         url = self.url_index + f"jobs/search?keywords={role}&location={location}"
 
         print('LinkedIn Scraper | INFO: Generating job list...')
 
-        job_list = self.get_job_list(url)
+        job_list = self.get_job_list(url, regex_matches)
 
         if not job_list:
             print('LinkedIn Scraper | ERROR: No jobs found during search.')
